f2df473cecd65edd96f8d43f736177bab8262851
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-11-08 18:16:46 -0300

    Configuration app core

--- enem_calculator_api/settings.py
+++ enem_calculator_api/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'enem_calculator_api.core',
-    'rest_framework'
+    'rest_framework',
+    'enem_calculator_api.core',
 ]
 
 MIDDLEWARE = [
